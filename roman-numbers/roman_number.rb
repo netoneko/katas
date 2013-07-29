8cf604d5b87929d3140b33590dbcba832447bc7b
Kirill Maksimov <k.maksimov@applicaster.com>
2013-07-29 19:57:19 +0300

    Insane gimmics"

--- roman-numbers/roman_number.rb
+++ roman-numbers/roman_number.rb
@@ -19,3 +19,9 @@ class RomanNumber
              40 => "XL", 50 => "L", 90 => "XC", 100 => "C",
              400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"}
 end
+
+class Integer
+  def to_roman
+    RomanNumber.new(self).value
+  end
+end
--- roman-numbers/roman_number_spec.rb
+++ roman-numbers/roman_number_spec.rb
@@ -1,29 +1,31 @@
 require "./roman_number"
 
 describe RomanNumber do
-  it { RomanNumber.new(1).value.should == "I" }
+  it { 1.to_roman.should == "I" }
 
-  it { RomanNumber.new(5).value.should == "V" }
+  it { 5.to_roman.should == "V" }
 
-  it { RomanNumber.new(10).value.should == "X" }
+  it { 10.to_roman.should == "X" }
 
-  it { RomanNumber.new(50).value.should == "L" }
+  it { 50.to_roman.should == "L" }
 
-  it { RomanNumber.new(100).value.should == "C" }
+  it { 100.to_roman.should == "C" }
 
-  it { RomanNumber.new(500).value.should == "D" }
+  it { 500.to_roman.should == "D" }
 
-  it { RomanNumber.new(1000).value.should == "M" }
+  it { 1000.to_roman.should == "M" }
 
-  it { RomanNumber.new(2000).value.should == "MM" }
+  it { 2000.to_roman.should == "MM" }
 
-  it { RomanNumber.new(76).value.should == "LXXVI" }
+  it { 76.to_roman.should == "LXXVI" }
 
-  it { RomanNumber.new(4).value.should == "IV" }
+  it { 4.to_roman.should == "IV" }
 
-  it { RomanNumber.new(79).value.should == "LXXIX" }
+  it { 79.to_roman.should == "LXXIX" }
 
-  it { RomanNumber.new(39).value.should == "XXXIX" }
+  it { 39.to_roman.should == "XXXIX" }
 
-  it { RomanNumber.new(1989).value.should == "MCMLXXXIX" }
+  it { 1989.to_roman.should == "MCMLXXXIX" }
+
+  it { 292.to_roman.should == "CCXCII" }
 end
