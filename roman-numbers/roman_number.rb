a52e0f61149d2aa5512fba8b02e5bc4d6e8833f5
Kirill Maksimov <k.maksimov@applicaster.com>
2013-07-29 19:44:43 +0300

    Works

---
+++ roman-numbers/roman_number.rb
@@ -0,0 +1,21 @@
+class RomanNumber
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def value
+    if NUMBERS.include? @num
+      NUMBERS[@num]
+    else
+      NUMBERS.reverse_each do |n, letter|
+        return [letter, RomanNumber.new(@num - n).value].join if @num >= n
+      end
+    end
+  end
+
+  NUMBERS = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X",
+             40 => "XL", 50 => "L", 90 => "XC", 100 => "C",
+             400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"}
+end
---
+++ roman-numbers/roman_number_spec.rb
@@ -0,0 +1,25 @@
+require "./roman_number"
+
+describe RomanNumber do
+  it { RomanNumber.new(1).value.should == "I" }
+
+  it { RomanNumber.new(5).value.should == "V" }
+
+  it { RomanNumber.new(10).value.should == "X" }
+
+  it { RomanNumber.new(50).value.should == "L" }
+
+  it { RomanNumber.new(100).value.should == "C" }
+
+  it { RomanNumber.new(500).value.should == "D" }
+
+  it { RomanNumber.new(1000).value.should == "M" }
+
+  it { RomanNumber.new(2000).value.should == "MM" }
+
+  it { RomanNumber.new(76).value.should == "LXXVI" }
+
+  it { RomanNumber.new(4).value.should == "IV" }
+
+  it { RomanNumber.new(79).value.should == "LXXIX" }
+end
